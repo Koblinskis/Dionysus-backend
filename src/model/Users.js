cc093f9288d286a8695c6d7de2759e3f58bab7bf
Micheal Koblinski <stevenkoblinskip@gmail.com>
2020-04-09 13:11:04 -0400

    removed unused code

--- src/model/Users.js
+++ src/model/Users.js
@@ -73,7 +73,6 @@ userSchema.statics.findByCredentials = async (password, email, userName) => {
 
 userSchema.pre('save', async function (next) {
   const user = this
-
   if(user.isModified('password')) {
     user.password = await bcrypt.hash(user.password, 8)
   }
